1644dce0cf03f115711545ee6a900e2cc80b5259
Harish Annam <harish.annam@gmail.com>
2020-04-20 18:10:05 +0530

    Set name, font, color, size etc

--- app/src/main/java/com/mobitalk/printyourname/MainActivity.kt
+++ app/src/main/java/com/mobitalk/printyourname/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.mobitalk.printyourname
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set your name below :)
+        your_name.text = "Harish Annam"
+        // Set text color - Available colors : BLACK, BLUE, CYAN, GRAY, GREEN, MAGENTA, RED, WHITE, YELLOW
+        your_name.setTextColor(Color.RED)
+        // Font size must be in float. So pick your desired font size such as 20f or 40f
+        your_name.textSize = 40f
+
+        // Setting Font - Check app/src/mail/assets/fonts folder for list of font names
+        val textFont = Typeface.createFromAsset(this.assets, "fonts/8bit.ttf")
+
+        // You can also set the font as BOLD, ITALIC, BOLD_ITALIC
+        your_name.setTypeface(textFont, Typeface.BOLD)
     }
 }
